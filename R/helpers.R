b410805022160c9da779c97979ebf371c9d56835
Antoine Fabri <antoine.fabri@gmail.com>
2019-02-23 19:31:54 +0100

    changed some letters, removed examples from page

--- R/helpers.R
+++ R/helpers.R
@@ -27,25 +27,25 @@ paste_enum <- function(..., sep = ", ", sep2 = " and "){
 
 safe_check <- function(x, y, by, check){
   # guess by and act depending on "j" check
-  j_check <- check_for_letter(check,"j")
-  if(is.null(by) && j_check$fun == "abort")
+  b_check <- check_for_letter(check,"b")
+  if(is.null(by) && b_check$fun == "abort")
     abort("`by`is `NULL`, joining columns should be explicit")
   by <- purrr::quietly(common_by)(by, x, y)
-  if(length(by$messages)) get(j_check$fun)(by$messages)
+  if(length(by$messages)) get(b_check$fun)(by$messages)
   by <- by$result
 
   # check x unicity
-  x_check <- check_for_letter(check,"x")
-  if(x_check$lgl && anyDuplicated(x[by$x])){
+  u_check <- check_for_letter(check,"u")
+  if(u_check$lgl && anyDuplicated(x[by$x])){
     txt <- sprintf("x is not unique on %s", paste_enum(by$x))
-    get(x_check)$fun(txt)
+    get(u_check$fun)(txt)
   }
 
   # check y unicity
-  y_check <- check_for_letter(check,"y")
-  if(x_check$lgl && anyDuplicated(y[by$y])){
+  v_check <- check_for_letter(check,"v")
+  if(u_check$lgl && anyDuplicated(y[by$y])){v
     txt <- sprintf("y is not unique on %s", paste_enum(by$y))
-    y_check$fun(txt)
+    get(v_check$fun)(txt)
   }
 
   # check column conflicts
@@ -55,6 +55,42 @@ safe_check <- function(x, y, by, check){
             intersect(setdiff(names(x),by$x), setdiff(names(y),by$y)))){
     txt <- sprintf("Conflict of auxiliary columns: %s", paste_enum(common_aux))
     get(c_check$fun)(txt)
+  }
+
+  # check if all combinations match on x
+  m_check <- check_for_letter(check,"m")
+  if(m_check$lgl &&
+     nrow( unmatched <- dplyr::setdiff(distinct(x[by$x]), setNames(distinct(y[by$y]),by$x)))){
+    txt <- paste("x has unmatched sets of joining values: \n%s",
+                 paste(capture.output(unmatched),collapse="\n"))
+    get(m_check$fun)(txt)
+  }
+
+  # check if all combinations match on y
+  n_check <- check_for_letter(check,"n")
+  if(n_check$lgl &&
+     nrow( unmatched <- dplyr::setdiff(distinct(y[by$y]), setNames(distinct(x[by$x]),by$y)))){
+    txt <- paste("y has unmatched sets of joining values: \n%s",
+                 paste(capture.output(unmatched),collapse="\n"))
+    get(n_check$fun)(txt)
+  }
+
+  # check if all combinations are present on x
+  e_check <- check_for_letter(check,"e")
+  if(e_check$lgl &&
+     nrow( absent_comb <- dplyr::setdiff(cross_df(d <- distinct(x[by$x])), d))){
+    txt <- paste("Some combinations of joining values are absent from x: \n%s",
+                 paste(capture.output(absent_comb),collapse="\n"))
+    get(e_check$fun)(txt)
+  }
+
+  # check if all combinations are present on x
+  f_check <- check_for_letter(check,"f")
+  if(f_check$lgl &&
+     nrow( absent_comb <- dplyr::setdiff(cross_df(d <- distinct(y[by$y])), d))){
+    txt <- paste("Some combinations of joining values are absent from y: \n%s",
+                 paste(capture.output(absent_comb),collapse="\n"))
+    get(f_check$fun)(txt)
   }
   by
 }
