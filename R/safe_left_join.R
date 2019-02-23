b4c373fcb18638633622d9a7b43b51aa5101ea75
Antoine Fabri <antoine.fabri@gmail.com>
2019-02-23 19:32:20 +0100

    updated doc

--- R/safe_left_join.R
+++ R/safe_left_join.R
@@ -1,7 +1,35 @@
-#' safe joins!
+#' Safe joins
+#'
+#' Wrappers around *dplyr*'s joining function that allow to check a variety of
+#' things on the fly and either `inform`, `warn` or `abort` as a result.
+#'
+#' `check` is a combination of characters which will trigger different checks:
+#' \describe{
+#'   \item{b}{as in **b**y, check that `by` was given explicitly. Default behavior
+#'     in *dplyr* is to trigger a message}
+#'   \item{c}{as in **c**olumn **c**onflict, check if, among non joining
+#'   columns, some column names are found in both `x` and `y`. Default behavior
+#'   in *dplyr*'s joining functions is to suffix them silently. }
+#'   \item{u}{as in **u**nique, check if no set of values of joining columns
+#'   is duplicated in `x`}
+#'   \item{v}{the letter after **u**, check if no set of values of joining columns
+#'   is duplicated in `y`}
+#'   \item{m}{as in **m**atch, check if all sets of values of joining columns in
+#'    `x` wil be matched in `y`}
+#'   \item{n}{the letter after **m**, check if all sets of values of joining columns in
+#'    `y` wil be matched in `x`}
+#'   \item{e}{as in **e**xpanded, check that all combinations of values of
+#'     joining columns are present in `x`}
+#'   \item{f}{the letter after **e**, check that all combinations of values of
+#'     joining columns are present in `y`}
+#' }
+#'
+#' An upper case letter will trigger `abort`, a lower case letter will trigger
+#'   `warn`, a lower case letter prefixed with `~` will trigger a message. Other
+#'   characters will be ignored.
 #'
 #' @inheritParams dplyr::join
-#' @param check a string
+#' @param check a string, see details
 #'
 #'
 #' @name safe_joins
@@ -79,11 +107,3 @@ safe_nest_join <- function(x, y, by = NULL, copy = FALSE, keep = FALSE,
   dplyr::nest_join(x, y, by = setNames(by$y,by$x), copy, keep, name, ...)
 }
 
-# safe_left_join(band_members,
-#                band_instruments %>% rename_at(2,~"band"),
-#                by = "name",
-#                check ="cj")
-#
-# safe_left_join(band_members,
-#                band_instruments,
-#                check ="cj")
