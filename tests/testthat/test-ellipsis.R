0e56550985e54b2b68b45fd78e8743371e06f64a
Antoine Fabri <antoine.fabri@gmail.com>
2019-02-26 02:24:01 +0100

    tests for ellipsis

--- tests/testthat/test-ellipsis.R
+++ tests/testthat/test-ellipsis.R
@@ -5,6 +5,10 @@ test_that("nothing unexpected is passed to dots", {
                "invalid column index")
   expect_error(eat(band_instruments, band_members, check = 3),
                "invalid column index")
+  expect_error(safe_left_join(band_instruments, band_members, .CHECK = 3))
+  expect_error(safe_left_join(band_instruments, band_members, .check = 3))
 })
 
 
+# debugonce(safe_check)
+# eat(band_instruments, band_members, conflict=coalesce)
