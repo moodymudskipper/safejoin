d8d298c8970ae3dce8f769013c25fab1b135ce8c
Antoine Fabri <antoine.fabri@gmail.com>
2019-03-24 01:26:39 +0100

    removed eat.list, added `::` in appropriate places + minor changes

--- R/03_eat.R
+++ R/03_eat.R
@@ -6,33 +6,32 @@
 #' Character codes are the same as for `safe_*_join` functions, with the
 #' addition of `"d"`to check if dots were filled.
 #'
-#' @inheritParams dplyr::left_join
-#' @inheritParams safe_joins
 #' @param .x,.y	tbls to join
-#' @param ... One or more unquoted expressions, passed to \code{dplyr::select},
-#'   defining the columns to keep from \code{y}
+#' @param ... One or more unquoted expressions, passed to `dplyr::select`,
+#'   defining the columns to keep from `.y`
 #' @param .by a character vector of variables to join by, or a formula defining
-#'   a joining condition using functions a notation such as
+#'   a joining condition using a notation such as
 #'   `~ X("var1") > Y("var2") & X("var3") < Y("var4")`
 #' @param .na_matches	 Use `"never"` to always treat two `NA` or `NaN` values as
 #'   different, like joins for database sources, similarly to
 #' @param .match_fun	 passed to `fuzzyjoin::fuzzy_join`. Vectorized function
 #'   given two columns, returning `TRUE` or `FALSE` as to whether they are a
 #'   match. Can be a list of functions one for each pair of columns specified in
-#'   by (if a named list, it uses the names in x). If only one function is given
+#'   by (if a named list, it uses the names in `.x`). If only one function is given
 #'    it is used on all column pairs.
-#' @param .agg function or formula or \code{NULL}, if not \code{NULL}, \code{y}
-#'   will be grouped by its \code{by} columns and \code{fun} will be applied to
-#'   all kept columns from {y}
+#' @param .agg function or formula or `NULL`, if not `NULL`, `.y`
+#'   will be grouped by its `.by` columns and `.agg` will be applied to
+#'   all kept columns from `.y`.
 #'
 #' @param .check a string, see details about `check` parameter in ?safe_joins
 #' @param .conflict if `NULL`, in case of column conflict both columns are
 #'   suffixed as in *dplyr*, if a function of two parameters or a formula,
-#'   a function is applied on both columns. If the string "patch", matched
-#'   values from `y` will overwrite existing values in `x` while the other
+#'   a function is applied on both columns. If the string `"patch"`, matched
+#'   values from `.y` will overwrite existing values in `.x` while the other
 #'   values will be kept`
 #' @param .prefix prefix of new columns or function/formula to apply on names of new
 #'   columns
+#' @param .fill if not `NULL`, value to fill missing values in eaten columns
 #' @param .mode type of join on build on, a left join by default
 #' @return a data frame
 #' @export
@@ -53,7 +52,7 @@ eat <- function(.x, .y, ..., .by = NULL,
   ############################
 
   if (is.list(.y) && !is.data.frame(.y)) {
-    dots <- enquos(...)
+    dots <- rlang::enquos(...)
     res <- purrr::reduce2(
       .y, rlang::names2(.y), .init = .x,
       ~ eat(
@@ -86,7 +85,7 @@ eat <- function(.x, .y, ..., .by = NULL,
   L <- safe_check(x = .x, y = .y, by = .by, match_fun = .match_fun,
                   check = .check, conflict = .conflict, in_eat = TRUE,
                   suffix = c(".x",".y"),
-                  agg = .agg, prefix = .prefix, dots = enquos(...)) #eval(substitute(alist(...))))
+                  agg = .agg, prefix = .prefix, dots = rlang::enquos(...)) #eval(substitute(alist(...))))
   with(L,{
     if (!is.null(.match_fun)) {
       ###############
@@ -94,7 +93,7 @@ eat <- function(.x, .y, ..., .by = NULL,
       ###############
       if (is.null(multi_by)) {
         # standard fuzzy join
-        res <- fuzzyjoin:::fuzzy_join(
+        res <- fuzzyjoin::fuzzy_join(
           x, y, by = `names<-`(by$y,by$x),
           match_fun = rlang::as_function(.match_fun),
           mode = .mode)
@@ -111,7 +110,7 @@ eat <- function(.x, .y, ..., .by = NULL,
       ##################
       # STANDARD JOINS #
       ##################
-      join <- getFromNamespace(paste0(.mode,"_join"), "dplyr")
+      join <- utils::getFromNamespace(paste0(.mode,"_join"), "dplyr")
       res <- join(
         x, y, by = `names<-`(by$y,by$x), na_matches = .na_matches)
     }
@@ -139,17 +138,17 @@ eat <- function(.x, .y, ..., .by = NULL,
 
 
 
-eat.list <- function(.x, .y, ..., .by = NULL,
-                     .na_matches = pkgconfig::get_config("dplyr::na_matches"),
-                     .agg = NULL,
-                     .check = "~blC",
-                     .conflict = NULL,
-                     .prefix = NULL,
-                     .mode = c("left","right","inner","full")){
-  Reduce(function(x,y) eat(x,y, ..., .by = .by, .agg = .agg, .check = .check,
-                           .conflict = .conflict, .prefix = .prefix,
-                           .join = .join),.y, .x)
-}
+# eat.list <- function(.x, .y, ..., .by = NULL,
+#                      .na_matches = pkgconfig::get_config("dplyr::na_matches"),
+#                      .agg = NULL,
+#                      .check = "~blC",
+#                      .conflict = NULL,
+#                      .prefix = NULL,
+#                      .mode = c("left","right","inner","full")){
+#   Reduce(function(x,y) eat(x,y, ..., .by = .by, .agg = .agg, .check = .check,
+#                            .conflict = .conflict, .prefix = .prefix,
+#                            .join = .join),.y, .x)
+# }
 #
 # debugonce(eat)
 # eat.list(band_instruments,list(band_members, band_members),.by="name", .prefix = "Z")
